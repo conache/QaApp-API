b9e12aeda64b8210de4cd41fa70bd839b878a552
VladEugeniu <h5_em_1@yahoo.com>
2020-01-26 20:39:12 +0200

    Null Tags Field for question

--- src/main/java/com/project/qa/model/elasticserach/QuestionAsResponse.java
+++ src/main/java/com/project/qa/model/elasticserach/QuestionAsResponse.java
@@ -16,7 +16,7 @@ public class QuestionAsResponse extends Question {
 
     public QuestionAsResponse(Question question, VoteStatus status)
     {
-        super(question.getQuestionAuthorId(),question.getGroupName(),question.getQuestionTitle(),question.getQuestionText(),question.getQuestionPublishDate(),null,null);
+        super(question.getQuestionAuthorId(),question.getGroupName(),question.getQuestionTitle(),question.getQuestionText(),question.getQuestionPublishDate(),null,question.getQuestionTags());
         this.voteStatus = status;
         this.modelId = question.getModelId();
         this.score = question.getScore();
