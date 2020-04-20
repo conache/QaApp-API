2d577db8b90e993487a5ca4cf2cb6908dc4fd277
Condition17 <c.conache@gmail.com>
2020-04-20 15:34:35 +0300

    Deploy scripts updated

--- deploy/deployNewVersion.sh
+++ deploy/deployNewVersion.sh
@@ -1,11 +1,11 @@
 # AWS login
-$(aws ecr get-login --region us-east-2 --no-include-email)
+$(aws ecr get-login --region eu-central-1 --no-include-email)
 
 # pull docker image for running the server application
-docker pull 972312342870.dkr.ecr.us-east-2.amazonaws.com/qa-platform:latest
+docker pull 972312342870.dkr.ecr.eu-central-1.amazonaws.com/qa-platform:latest
 
 # kill all running containers
 docker container kill $(docker ps -q)
 
 # run the new container
-docker run -d -p 8080:8085 972312342870.dkr.ecr.us-east-2.amazonaws.com/qa-platform:latest
+docker run -d -p 8080:8085 972312342870.dkr.ecr.eu-central-1.amazonaws.com/qa-platform:latest
